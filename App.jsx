c221c71f5fc9e43449a11f9222218f0186e8bbd9
nitinlathwal <nitin00lathwal@gmail.com>
2026-03-18 16:50:41 +0530

    the more changes

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider, useDispatch } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { NavigationContainer } from "@react-navigation/native";
+import ProductNavigator from "./navigation/ProductNavigator.jsx";
+
+function App() {
+  const isDarkMode = useColorScheme() === "light";
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <StatusBar
+              barStyle={isDarkMode ? "light-content" : "dark-content"}
+            />
+            <ProductNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-export { default as ProductNavigator } from "./navigation/ProductNavigator";
-export { default as ProductList } from "./screens/ProductList";
-export { default as ProductDetails } from "./screens/ProductDetails";
-export { default as OsaRequests } from "./screens/OsaRequests";
-export { default as ProductsScan } from "./screens/ProductsScan";
-export { default as ProductsPerCategory } from "./screens/ProductsPerCategory";
-export { default as AllOsaRequests } from "./screens/AllOsaRequests";
-export { default as Scanner } from "./screens/Scanner";
-export { store, persistor } from "./redux/store";
-// index.js of module
-export { default as productAppReducer } from "./redux/reducers";
+// export { default as ProductNavigator } from "./navigation/ProductNavigator";
+// export { default as ProductList } from "./screens/ProductList";
+// export { default as ProductDetails } from "./screens/ProductDetails";
+// export { default as OsaRequests } from "./screens/OsaRequests";
+// export { default as ProductsScan } from "./screens/ProductsScan";
+// export { default as ProductsPerCategory } from "./screens/ProductsPerCategory";
+// export { default as AllOsaRequests } from "./screens/AllOsaRequests";
+// export { default as Scanner } from "./screens/Scanner";
+// export { store, persistor } from "./redux/store";
+// // index.js of module
+// export { default as productAppReducer } from "./redux/reducers";
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+AppRegistry.registerComponent(appName, () => App);
+
